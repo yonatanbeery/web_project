c87a314de56562f2ca28ee502a58fc3ecf73cb4e
yonatanbeery <yonatanbeery@gmail.com>
2024-01-05 22:48:56 +0200

    can upload image now

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -3,22 +3,35 @@ import { useContext, useState } from "react";
 import {AuthContext} from "../App";
 
 const Signup = () => {
-    const [username, setUsername] = useState<String>();
-    const [password, setPassword] = useState<String>();
+    const [userImage, setUserImage] = useState<string>("../../public/defaultUserImage.png");
+    const [username, setUsername] = useState<string>();
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const [confirmPassword, setConfirmPassword] = useState<string>();
+    const [isSubmitted, setIsSubmitted] = useState<boolean>();
     const {setAuthToken} = useContext(AuthContext);
+
+    const signupNewUser = () => {
+        setIsSubmitted(true);
+    }
+
+    const onFileChange = (event: any) => {    
+        setUserImage(URL.createObjectURL(event.target.files[0]))
+    };
 
     return (
         <Card sx={{ minWidth: '100vh', height: '75vh', marginTop:'5vh' }}>
             <Grid container spacing={1}>
                 <Grid xs={12} sx={{marginTop: 5, justifyContent:'center', alignItems: 'center', display:'flex', flexDirection:'column'}}>
-                    <Button>
-                        <Box component="img" src="../../public/defaultUserImage.png" sx={{height: 125, width: 125}}></Box>
-                    </Button>
-                    <TextField onChange={(prop) => setUsername(prop.target.value)} id="Username" label="Username" variant="outlined" sx={{margin: 1, width: '50vh'}} />
-                    <TextField onChange={(prop) => setUsername(prop.target.value)} id="Email" label="Email" variant="outlined" sx={{margin: 1, width: '50vh'}} />
-                    <TextField onChange={(prop) => setPassword(prop.target.value)} id="Password" label="Password" variant="outlined" type="password" sx={{margin: 1, width: '50vh'}} />
-                    <TextField onChange={(prop) => setPassword(prop.target.value)} id="Confirm password" label="Confirm password" variant="outlined" type="password" sx={{margin: 1, width: '50vh'}} />
-                    <Button onClick={() => setAuthToken("")} color="primary" variant="contained" sx={{marginRight: 20, marginLeft: 20, marginTop: 4}}>Submit</Button>
+                    <Box>
+                        <Box component="img" src={userImage} sx={{height: 125, width: 125}}/>
+                        <input type="file" onChange={onFileChange}/>
+                    </Box>
+                    <TextField error={!username && isSubmitted} onChange={(prop) => setUsername(prop.target.value)} id="Username" label="Username" variant="outlined" sx={{margin: 1, width: '50vh'}} />
+                    <TextField error={!email && isSubmitted} onChange={(prop) => setEmail(prop.target.value)} id="Email" label="Email" variant="outlined" sx={{margin: 1, width: '50vh'}} />
+                    <TextField error={!password && isSubmitted} onChange={(prop) => setPassword(prop.target.value)} id="Password" label="Password" variant="outlined" type="password" sx={{margin: 1, width: '50vh'}} />
+                    <TextField error={!confirmPassword && isSubmitted} onChange={(prop) => setConfirmPassword(prop.target.value)} id="Confirm password" label="Confirm password" variant="outlined" type="password" sx={{margin: 1, width: '50vh'}} />
+                    <Button onClick={signupNewUser} color="primary" variant="contained" sx={{marginRight: 20, marginLeft: 20, marginTop: 4}}>Sign up</Button>
                 </Grid>
             </Grid>
         </Card>
