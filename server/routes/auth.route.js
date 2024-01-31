62d5cdc3f09c840b93d0a9f9fe62251b69a31cc6
yonatanbeery <yonatanbeery@gmail.com>
2024-01-31 21:27:42 +0200

    first schema

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -4,6 +4,34 @@ const Users = require("../controllers/auth.controller");
 const multer = require('multer');
 let upload = multer({ dest: 'photos/users/' })
 
+/**
+* @swagger
+* tags:
+*   name: Auth
+*   description: The Authentication API
+* components:
+*   securitySchemes:
+*       bearerAuth:
+*           type: http
+*           scheme: bearer
+*           bearerFormat: JWT
+*   schemas:
+*       User:
+*           type: object
+*           required:
+*               - email
+*               - password
+*           properties:
+*               email:
+*                   type: string
+*                   description: The user email
+*               password:
+*                   type: string
+*                   description: The user password
+*           example:
+*               email: 'bob@gmail.com'
+*               password: '123456'
+*/
 router.post("/login", Users.login);
 
 router.post("/googleLogin", Users.googleLogin);
