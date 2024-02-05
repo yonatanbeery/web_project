920bc3aa52b0f9318755f21c81721b44d7dcacea
yonatanbeery <yonatanbeery@gmail.com>
2024-02-05 19:59:51 +0200

    changed localhost to ip

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
+// var fs = require('fs');
+// var http = require('http');
+// var https = require('https');
+const env = require("dotenv").config();
 const initApp = require("./app");
 
 initApp().then((app) => {
-  const port = process.env.PORT;
-  app.listen(port, () => {
-    console.log(`server listening at http://localhost:${port}`);
-  });
+  if (process.env.NODE_ENV == "development") {
+    const port = process.env.PORT;
+    app.listen(port, () => {
+      console.log(`server listening at http://localhost:${port}`);
+    });
+  } else {
+    var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+    var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+  }
+  
 });
