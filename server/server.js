e0dcca33dcd63e2d77589a17b7a7a39da9a78a4f
yonatanbeery <yonatanbeery@gmail.com>
2024-02-06 20:03:20 +0200

    updating env files

--- server/server.js
+++ server/server.js
@@ -1,19 +1,24 @@
-// var fs = require('fs');
-// var http = require('http');
-// var https = require('https');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 const env = require("dotenv").config();
 const initApp = require("./app");
 
 initApp().then((app) => {
   if (process.env.NODE_ENV == "development") {
-    const port = process.env.PORT;
-    app.listen(port, () => {
+    const port = process.env.HTTP_PORT;
+    var httpServer = http.createServer(app);
+    httpServer.listen(port, () => {
       console.log(`server listening at http://localhost:${port}`);
     });
-  } else {
-    var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-    var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-    var credentials = {key: privateKey, cert: certificate};
   }
-  
+    const port = process.env.HTTPS_PORT;
+    var privateKey  = fs.readFileSync('./client-key.pem', 'utf8');
+    var certificate = fs.readFileSync('./client-cert.pem', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+    var httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => {
+      console.log(`server listening at https://localhost:${port}`);
+    });
 });
+
